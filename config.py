fa1a969c85085a486679bef5f858fb06a99f7a01
Rachita <rachitadhiman.rd@gmail.com>
2026-04-25 10:39:12 +0530

    Fix database path and setuptools for Render

--- config.py
+++ config.py
@@ -2,6 +2,11 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'dev-key-12345'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///foodie.db'
+    
+    # Use absolute path for SQLite to work on Render
+    basedir = os.path.abspath(os.path.dirname(__file__))
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'instance', 'foodie.db')
+    
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_ECHO = False
